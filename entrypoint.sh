30b9f07e2b6332e7d21ba156c3f45ddd73a155a8
ech0p1ng <alexey2003petrov@yandex.ru>
2025-12-16 10:49:15 +0500

    alembic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# poetry run alembic upgrade head
+poetry run alembic upgrade head
 # poetry run uvicorn src.main:app --reload --host 0.0.0.0 --port 8080
 poetry run watchfiles --filter python "poetry run python src/main.py" src/
